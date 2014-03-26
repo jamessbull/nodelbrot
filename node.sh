90f4b0f7555fb1f60d9a3f7d053a1ca8764a3c0f
Jim B <James.Bull@lloydsbanking.com>
2014-03-26 14:01:02 +0000

    Fix shell script to work for first time checkout

--- node.sh
+++ node.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+if ! [ -d "./toolchain" ]
+then
+    echo "Toolchain not present - creating ./toolchain"
+    mkdir toolchain
+fi
+
 if ! [ -d "./toolchain/node-v0.10.26" ]
 then
-    echo "Unpacking node"
+    echo "Node does not exist in toolchain - Unpacking node"
     cd toolchain
     tar xzf ../bin/node.tar.gz
     cd ..
 fi
 echo "Starting node"
-toolchain/node-v0.10.26/bin/node $1
-
-
+./toolchain/node-v0.10.26/bin/node $1
 
