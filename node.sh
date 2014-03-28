3418deccac0094a28f8bedc89e54eadab50cdc03
Jim B <James.Bull@lloydsbanking.com>
2014-03-28 15:35:02 +0000

    Added initial webdriver test. We can now run a simple test. Woo!

--- node.sh
+++ node.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+home=`pwd`
+
+export PATH=$PATH:$home/toolchain
+export NODE_PATH=$home/toolchain/node-v0.10.26/lib/node_modules
 
 if ! [ -d "./toolchain" ]
 then
@@ -6,21 +10,21 @@ then
     mkdir toolchain
 fi
 
-if ! [ -d "./toolchain/chrome-driver" ]
+if ! [ -d "$home/toolchain/chrome-driver" ]
 then
     echo "Chromedriver does not exist in toolchain - Unpacking chromedriver"
-    cd toolchain
-    tar xzf "../bin/chromedriver.tar.gz"
-    cd ..
+    cd $home/toolchain
+    tar xzf "$home/bin/chromedriver.tar.gz"
+    cd $home
 fi
 
-if ! [ -d "./toolchain/node-v0.10.26" ]
+if ! [ -d "$home/toolchain/node-v0.10.26" ]
 then
     echo "Node does not exist in toolchain - Unpacking node"
-    cd toolchain
+    cd $home/toolchain
     tar -Pxzf ../bin/node.tar.gz
     cd ..
 fi
 echo "Starting node"
-./toolchain/node-v0.10.26/bin/node $1
+$home/toolchain/node-v0.10.26/bin/node $1
 
