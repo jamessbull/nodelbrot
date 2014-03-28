f8ea03307be8b3d723dbecf824eb71b07365819e
Jim B <James.Bull@lloydsbanking.com>
2014-03-28 11:46:26 +0000

    Included handlebars webdriver and node jasmine in the node install. Also updated the setup script to unpack chromedriver to the toolchain directory.

--- node.sh
+++ node.sh
@@ -6,11 +6,19 @@ then
     mkdir toolchain
 fi
 
+if ! [ -d "./toolchain/chrome-driver" ]
+then
+    echo "Chromedriver does not exist in toolchain - Unpacking chromedriver"
+    cd toolchain
+    tar xzf "../bin/chromedriver.tar.gz"
+    cd ..
+fi
+
 if ! [ -d "./toolchain/node-v0.10.26" ]
 then
     echo "Node does not exist in toolchain - Unpacking node"
     cd toolchain
-    tar xzf ../bin/node.tar.gz
+    tar -Pxzf ../bin/node.tar.gz
     cd ..
 fi
 echo "Starting node"
